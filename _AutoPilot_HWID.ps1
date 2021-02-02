a207185dcfe5933044d50aae2bb209e7176f071a
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-02-02 13:51:57 +0100

    Add files via upload

---
+++ _AutoPilot_HWID.ps1
@@ -0,0 +1,5 @@
+﻿New-Item -Type Directory -Path "C:\HWID"
+Set-Location -Path "C:\HWID"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
+Install-Script -Name Get-WindowsAutoPilotInfo
+Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
