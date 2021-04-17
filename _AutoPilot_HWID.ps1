b679b8887b61d709d1ea88c6fe3026c6f83e36f3
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-04-17 14:31:23 +0200

    Update _AutoPilot_HWID.ps1

--- _AutoPilot_HWID.ps1
+++ _AutoPilot_HWID.ps1
@@ -3,3 +3,6 @@ Set-Location -Path "C:\HWID"
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 Install-Script -Name Get-WindowsAutoPilotInfo
 Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
+
+net use z:\\172.16.1.10\temp /user:tomsmem\administrator
+copy AutoPilotHWID.csv z:\AutoPilotHWID.csv
