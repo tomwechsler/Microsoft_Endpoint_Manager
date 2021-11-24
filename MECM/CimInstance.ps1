78c14bf29f1cc60c01f045e068c747aafe8b0cc8
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-11-24 07:22:53 +0100

    Update CimInstance.ps1

--- MECM/CimInstance.ps1
+++ MECM/CimInstance.ps1
@@ -5,3 +5,5 @@ Get-CimInstance Win32_ComputerSystem | fl *
 Get-CimInstance Win32_BootConfiguration | fl *
 
 Get-CimInstance Win32_ComputerSystemProduct | fl *
+
+Get-CimInstance Win32_ComputerSystemProcessor | fl *
