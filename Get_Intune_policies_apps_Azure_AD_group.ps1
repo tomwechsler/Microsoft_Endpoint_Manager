ed995b813dd644bec125cb4b092fa27a409bcb36
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-02-27 07:12:23 +0100

    Add files via upload

---
+++ Get_Intune_policies_apps_Azure_AD_group.ps1
@@ -0,0 +1,148 @@
+﻿#A special thanks to Timmy Andersson for the PowerShell support!
+
+Set-Location C:\
+Clear-Host
+
+#Install the module
+Install-Module -Name Microsoft.Graph.Intune -AllowClobber -Verbose -Force
+
+#Connect and change schema 
+Connect-MSGraph -ForceInteractive
+Update-MSGraphEnvironment -SchemaVersion beta
+Connect-MSGraph
+ 
+#Which AAD group do we want to check against
+$groupName = "AutoPilot Geräte"
+ 
+#$Groups = Get-AADGroup | Get-MSGraphAllPages
+$Group = Get-AADGroup -Filter "displayname eq '$GroupName'"
+ 
+####Config Don't change
+ 
+Write-host "AAD Group Name: $($Group.displayName)" -ForegroundColor Green
+ 
+#Apps
+$AllAssignedApps = Get-IntuneMobileApp -Filter "isAssigned eq true" -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Apps found: $($AllAssignedApps.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllAssignedApps) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+#Device Compliance
+$AllDeviceCompliance = Get-IntuneDeviceCompliancePolicy -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Compliance policies found: $($AllDeviceCompliance.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllDeviceCompliance) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+#Device Configuration
+$AllDeviceConfig = Get-IntuneDeviceConfigurationPolicy -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Configurations found: $($AllDeviceConfig.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllDeviceConfig) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+#Device Configuration Powershell Scripts 
+$Resource = "deviceManagement/deviceManagementScripts"
+$graphApiVersion = "Beta"
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)?`$expand=groupAssignments"
+$DMS = Invoke-MSGraphRequest -HttpMethod GET -Url $uri
+$AllDeviceConfigScripts = $DMS.value | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Configurations Powershell Scripts found: $($AllDeviceConfigScripts.DisplayName.Count)" -ForegroundColor cyan
+ 
+Foreach ($Config in $AllDeviceConfigScripts) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+ 
+#Administrative templates
+$Resource = "deviceManagement/groupPolicyConfigurations"
+$graphApiVersion = "Beta"
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)?`$expand=Assignments"
+$ADMT = Invoke-MSGraphRequest -HttpMethod GET -Url $uri
+$AllADMT = $ADMT.value | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Administrative Templates found: $($AllADMT.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllADMT) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+
+#Lets check against all groups
+
+$Groups = Get-AADGroup | Get-MSGraphAllPages
+ 
+####Config 
+Foreach ($Group in $Groups) {
+Write-host "AAD Group Name: $($Group.displayName)" -ForegroundColor Green
+ 
+#Apps
+$AllAssignedApps = Get-IntuneMobileApp -Filter "isAssigned eq true" -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Apps found: $($AllAssignedApps.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllAssignedApps) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+#Device Compliance
+$AllDeviceCompliance = Get-IntuneDeviceCompliancePolicy -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Compliance policies found: $($AllDeviceCompliance.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllDeviceCompliance) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+#Device Configuration
+$AllDeviceConfig = Get-IntuneDeviceConfigurationPolicy -Select id, displayName, lastModifiedDateTime, assignments -Expand assignments | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Configurations found: $($AllDeviceConfig.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllDeviceConfig) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+#Device Configuration Powershell Scripts 
+$Resource = "deviceManagement/deviceManagementScripts"
+$graphApiVersion = "Beta"
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)?`$expand=groupAssignments"
+$DMS = Invoke-MSGraphRequest -HttpMethod GET -Url $uri
+$AllDeviceConfigScripts = $DMS.value | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Configurations Powershell Scripts found: $($AllDeviceConfigScripts.DisplayName.Count)" -ForegroundColor cyan
+ 
+Foreach ($Config in $AllDeviceConfigScripts) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+ 
+ 
+#Administrative templates
+$Resource = "deviceManagement/groupPolicyConfigurations"
+$graphApiVersion = "Beta"
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)?`$expand=Assignments"
+$ADMT = Invoke-MSGraphRequest -HttpMethod GET -Url $uri
+$AllADMT = $ADMT.value | Where-Object {$_.assignments -match $Group.id}
+Write-host "Number of Device Administrative Templates found: $($AllADMT.DisplayName.Count)" -ForegroundColor cyan
+Foreach ($Config in $AllADMT) {
+ 
+Write-host $Config.displayName -ForegroundColor Yellow
+ 
+}
+ 
+}
