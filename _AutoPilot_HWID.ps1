44f98d166189683bcbd220bad9397c5dc6f79450
Tom Wechsler <tom.wechsler@outlook.com>
2023-07-10 06:48:22 +0200

    lines edited

--- _AutoPilot_HWID.ps1
+++ _AutoPilot_HWID.ps1
@@ -4,5 +4,9 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 Install-Script -Name Get-WindowsAutoPilotInfo
 Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
 
+#Copy the file to another system
 net use z: \\192.168.179.10\temp /user:tomsmem\administrator
 copy AutoPilotHWID.csv z:\AutoPilotHWID.csv
+
+#Direct upload to intune
+Get-WindowsAutoPilotInfo.ps1 /online
